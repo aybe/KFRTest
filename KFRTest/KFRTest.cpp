05c6febc7c07ffdd883fb0514262429da8dfcedc
aybe <aybe@users.noreply.github.com>
2023-09-29 23:51:33 +0200

    Setup stuff.

--- KFRTest/KFRTest.cpp
+++ KFRTest/KFRTest.cpp
@@ -1,48 +1,52 @@
 #include <format>
 #include <iostream>
+#include <vector>
 #include <kfr/all.hpp>
 
-// ReSharper disable CppInconsistentNaming
 // ReSharper disable IdentifierTypo
 
 int main()
 {
-	using cometa::fbase;
 	using namespace kfr;
 
-	auto source = open_file_for_reading("C:\\temp\\white-noise.wav");
-	auto target = open_file_for_writing("C:\\temp\\white-noise-filter.wav");
+	auto source_file = open_file_for_reading(KFR_FILEPATH("C:\\temp\\messij.wav"));
+	auto target_file = open_file_for_writing(KFR_FILEPATH("C:\\temp\\messij-reverb.wav"));
 
-	auto reader = kfr::audio_reader_wav<fbase>(source);
+	auto source_reader = audio_reader_wav<double>(source_file);
 
-	auto& format = reader.format();
+	auto source_format = source_reader.format();
 
-	auto writer = kfr::audio_writer_wav<fbase>(target, format);
+	auto source_rate = source_format.samplerate;
+	auto target_rate = source_rate; // TODO
 
-	const auto channels = format.channels;
+	auto target_format = source_format;
 
-	univector<fbase, 461> taps;
-	constexpr double cutoff = 0.25;
-	const auto window = to_handle(window_blackman_harris(taps.size()));
-	fir_lowpass(taps, cutoff, window, true);
-	auto filters = std::vector(channels, kfr::filter_fir<fbase>(taps));
+	target_format.samplerate = target_rate;
 
-	constexpr auto block_size = static_cast<size_t>(1024);
-	auto blocks = static_cast<size_t>(std::ceil(static_cast<double>(format.length) / block_size));
+	auto target_writer = audio_writer_wav<double>(target_file, target_format);
+
+	auto length = source_format.length;
+
+	auto block_size = static_cast<size_t>(1024);
+	auto blocks = static_cast<size_t>(std::ceil(static_cast<double>(length) / static_cast<double>(block_size)));
+
+	auto channels = source_format.channels;
+
+	auto resamplers = std::vector(
+		channels,
+		resampler<double>(sample_rate_conversion_quality::high, static_cast<size_t>(target_rate), static_cast<size_t>(source_rate))
+	);
+
+	auto source_length = block_size * channels;
+	auto target_length = block_size * static_cast<size_t>(target_rate) / static_cast<size_t>(source_rate) * channels;
+	auto source_buffer = univector<double>(source_length);
+	auto target_buffer = univector<double>(target_length);
+	auto sample_buffer = univector2d<double, 2, 4>();
 
 	for (auto block = 0u; block < blocks; ++block)
 	{
-		auto data = reader.read_channels(block_size);
-
-		for (auto channel = 0u; channel < channels; ++channel)
-		{
-			auto& filter = filters[channel];
-			auto& vector = data[channel];
-			filter.apply(vector);
-		}
-
-		writer.write_channels(data);
-
-		std::cout << std::format("Processed block {} of {}\n", block + 1, blocks);
+		size_t r_size = source_reader.read(source_buffer);
+		size_t w_size = target_writer.write(source_buffer.data(), r_size);
+		std::cout << std::format("Block {} of {}\n", block + 1, blocks);
 	}
 }
