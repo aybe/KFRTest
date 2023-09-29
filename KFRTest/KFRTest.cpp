28b7bad0d7f9285206c70b6354a821ef35c485c5
aybe <aybe@users.noreply.github.com>
2023-09-29 20:08:59 +0200

    Add basic filtering.

--- KFRTest/KFRTest.cpp
+++ KFRTest/KFRTest.cpp
@@ -1,20 +1,48 @@
-// KFRTest.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <format>
 #include <iostream>
+#include <kfr/all.hpp>
+
+// ReSharper disable CppInconsistentNaming
+// ReSharper disable IdentifierTypo
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	using cometa::fbase;
+	using namespace kfr;
+
+	auto source = open_file_for_reading("C:\\temp\\white-noise.wav");
+	auto target = open_file_for_writing("C:\\temp\\white-noise-filter.wav");
+
+	auto reader = kfr::audio_reader_wav<fbase>(source);
+
+	auto& format = reader.format();
+
+	auto writer = kfr::audio_writer_wav<fbase>(target, format);
+
+	const auto channels = format.channels;
+
+	univector<fbase, 461> taps;
+	constexpr double cutoff = 0.25;
+	const auto window = to_handle(window_blackman_harris(taps.size()));
+	fir_lowpass(taps, cutoff, window, true);
+	auto filters = std::vector(channels, kfr::filter_fir<fbase>(taps));
+
+	constexpr auto block_size = static_cast<size_t>(1024);
+	auto blocks = static_cast<size_t>(std::ceil(static_cast<double>(format.length) / block_size));
+
+	for (auto block = 0u; block < blocks; ++block)
+	{
+		auto data = reader.read_channels(block_size);
+
+		for (auto channel = 0u; channel < channels; ++channel)
+		{
+			auto& filter = filters[channel];
+			auto& vector = data[channel];
+			filter.apply(vector);
+		}
+
+		writer.write_channels(data);
+
+		std::cout << std::format("Processed block {} of {}\n", block + 1, blocks);
+	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
